38c64ed23dd438d2ab8772300d826a9648e864ad
eagle1111 <wang499817054@gmail.com>
2017-06-22 19:57:42 +0800

    add model

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ApplicationRecord
    validates_uniqueness_of :friendly_id
    validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
    before_validation :generate_friendly_id, :on => :create
+   has_many :tickets, :dependent => :destroy
 
    STATUS = ["draft", "public", "private"]
    validates_inclusion_of :status, :in => STATUS
