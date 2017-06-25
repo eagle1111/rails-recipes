2567102624692e5d7d6c2236daa2d362061f6535
eagle1111 <wang499817054@gmail.com>
2017-06-25 22:03:04 +0800

    前台活动状态筛选

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,6 +12,9 @@ class Event < ApplicationRecord
    ranks :row_order
    STATUS = ["draft", "public", "private"]
    validates_inclusion_of :status, :in => STATUS
+
+   scope :only_public, -> { where( :status => "public" ) }
+   scope :only_available, -> { where( :status => ["public", "private"] ) }
    def to_param
      self.friendly_id
    end
