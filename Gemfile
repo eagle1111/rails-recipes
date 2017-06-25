9f90ef2a452e432378d1fc3158727520f9c273a7
eagle1111 <wang499817054@gmail.com>
2017-06-25 10:15:16 +0800

    ranked-model

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "select2-rails"
 gem "nested_form_fields"
 gem 'bootstrap-datepicker-rails'
 gem 'ckeditor'
+gem 'ranked-model'
 group :development, :test do
   gem 'rspec-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,8 @@ class Event < ApplicationRecord
    before_validation :generate_friendly_id, :on => :create
    has_many :tickets, :dependent => :destroy
    accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
-
+   include RankedModel
+   ranks :row_order
    STATUS = ["draft", "public", "private"]
    validates_inclusion_of :status, :in => STATUS
    def to_param
