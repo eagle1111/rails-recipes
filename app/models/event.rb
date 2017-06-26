8122e5f81fd570f9385f4d13181f27a6ca95dcc6
eagle1111 <wang499817054@gmail.com>
2017-06-26 12:08:46 +0800

    上传多图

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,6 +17,9 @@ class Event < ApplicationRecord
    scope :only_available, -> { where( :status => ["public", "private"] ) }
 
    mount_uploader :logo, EventLogoUploader
+
+   mount_uploaders :images, EventImageUploader
+   serialize :images, JSON
    def to_param
      self.friendly_id
    end
